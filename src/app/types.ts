f4795474ef9caca4639779f8e67a0a37c8e8f512
Ignacio Cisternas <105437306+nachodev-ui@users.noreply.github.com>
2026-07-14 01:44:31 -0400

    feat: add admin application route

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,5 +1,5 @@
 export type AppModule = "crafting" | "refining" | "presets";
-export type AppRoute = AppModule | "plans" | "account";
+export type AppRoute = AppModule | "plans" | "account" | "admin";
 
 export function isAppModule(route: AppRoute): route is AppModule {
   return route === "crafting" || route === "refining" || route === "presets";
